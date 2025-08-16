4c161deadc21b3ca2b166ffa6a0c5ea1f6e2ac89
OSCARKILIKYEUNG <oscarkilik@gmail.com>
2025-08-17 01:37:13 +0800

    Update routing.ts

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -1,6 +1,9 @@
+// src/i18n/routing.ts
 import {defineRouting} from 'next-intl/routing';
 
 export const routing = defineRouting({
+  // 你的語言清單
   locales: ['en', 'zh-Hant'],
-  defaultLocale: 'zh-Hant'
+  // 預設語言
+  defaultLocale: 'en'
 });
